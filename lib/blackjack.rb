031af36559abfdbb8f48e2a4a68d051f2b836b83
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-20 02:57:15 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -30,17 +30,17 @@ def initial_round
   return sum
 end
 
-def hit?(total)
-  prompt_user
-  user_input = get_user_input
-  if user_input == "h"
-    total += deal_card
-  elsif user_input == "s"
-    total
-  else
-    invalid_command
-  end 
-end
+# def hit?(total)
+#   prompt_user
+#   user_input = get_user_input
+#   if user_input == "h"
+#     total += deal_card
+#   elsif user_input == "s"
+#     total
+#   else
+#     invalid_command
+#   end 
+# end
 
 def invalid_command
   puts "Please enter a valid command"
