3e49182e3812012620c2ff47c74faf9162e46acd
Johnnie Gonzalez <johnnie.agonz71@gmail.com>
2020-08-20 02:42:22 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,8 +35,8 @@ def hit?(total)
   get_user_input
   if get_user_input == "h"
     total += deal_card
-    elsif get_user_input == "s"
-    puts 
+  else get_user_input == "s"
+    puts "Please enter a valid command"
   end 
   total
 end
